8358a58f66184ef96bce5226c7a03f5ca57872c7
Alexander Goodkind <alex@goodkind.io>
2026-05-11 18:59:29 -0700

    Extend CompactPreview to cover metrics-only mode

--- internal/compact/runtime_types.go
+++ internal/compact/runtime_types.go
@@ -30,24 +30,27 @@ type RuntimeRequest struct {
 }
 
 type RuntimeUpfront struct {
-	SessionName     string
-	SessionID       string
-	Model           string
-	CurrentTotal    int
-	MaxTokens       int
-	Messages        int
-	CompactBuffer   int
-	Free            int
-	ContextOverhead int
-	Target          int
-	StaticFloor     int
-	Reserved        int
-	Thinking        int
-	Images          int
-	ToolPairs       int
-	ChatTurns       int
-	StrippersText   string
-	TargetDate      string
+	SessionName         string
+	SessionID           string
+	Model               string
+	CurrentTotal        int
+	MaxTokens           int
+	Messages            int
+	CompactBuffer       int
+	Free                int
+	ContextOverhead     int
+	Target              int
+	StaticFloor         int
+	Reserved            int
+	Thinking            int
+	Images              int
+	ToolPairs           int
+	ChatTurns           int
+	StrippersText       string
+	TargetDate          string
+	PostBoundaryEntries int
+	Calibrated          bool
+	CalibrationOverhead int
 }
 
 type RuntimeIteration struct {
