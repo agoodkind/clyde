a2af1c155d8b35e17d0d2addcf9aa2913a8e839d
Alexander Goodkind <alex@goodkind.io>
2026-06-30 10:37:29 -0700

    Fix hookspec sentinel pointer method references

--- internal/hookspec/stack_refs.go
+++ internal/hookspec/stack_refs.go
@@ -10,6 +10,7 @@ import (
 var stackRefsCursorDocument = &cursorHooksDocument{fields: nil}
 
 var (
+<<<<<<< HEAD
 	_    = shellCommand
 	_    = shellQuote
 	_    = isShellBareWord
@@ -51,6 +52,123 @@ var (
 	_    = SupportedClients()
 	_    = writeAdditionalContext(io.Discard, ClientClaudeCode, "", "")
 	_    = writeCursorFollowup(io.Discard, "")
+||||||| parent of 86f605bf (Fix hookspec sentinel pointer method references)
+	_ = shellCommand
+	_ = shellQuote
+	_ = isShellBareWord
+	_ = SupportedClients
+	_ = writeAdditionalContext
+	_ = writeCursorFollowup
+	_ = NewFileSnapshotStore
+	_ = FileSnapshotStore.Save
+	_ = FileSnapshotStore.Consume
+	_ = normalizeSnapshotKey
+	_ = marshalCodexHookInstalls
+	_ = removeCodexCommandHookGroups
+	_ = isCodexHookGroupHeader
+	_ = isNonHookTableHeader
+	_ = codexGroupContainsCommand
+	_ = parseTomlAssignment
+	_ = removeCodexManagedBlock
+	_ = ensureCodexHooksFeature
+	_ = isTomlTableHeader
+	_ = renderCodexManagedBlock
+	_ = codexCommandSignatures
+	_ = codexEventName
+	_ = marshalCodexTrustedHash
+	_ = unmarshalCursorHooksDocument
+	_ = cursorHooksDocument{}.MarshalJSON
+	_ = cursorHooksDocument{}.marshalCursorHookInstalls
+	_ = cursorHooksDocument{}.unmarshalCursorHooks
+	_ = cursorHooksDocument{}.setInt
+	_ = cursorCommandSignatures
+	_ = removeCursorHookHandlers
+	_ = newCursorCommandHookHandler
+	_ = rawCursorHookHandler{}.MarshalJSON
+	_ = rawCursorHookHandler{}.UnmarshalJSON
+	_ = rawCursorHookHandler{}.command
+	_ = rawCursorHookHandler{}.setString
+	_ = rawCursorHookHandler{}.setInt
+	_ = rawCursorHookHandler{}.setBool
+)
+
+func init() {
+	_ = shellCommand("", nil)
+	_ = SupportedClients()
+	_ = writeAdditionalContext(io.Discard, ClientClaudeCode, "", "")
+	_ = writeCursorFollowup(io.Discard, "")
+
+	store := NewFileSnapshotStore(filepath.Join(os.TempDir(), "clyde-hookspec-reachability"))
+	key := ReorientSnapshotKey{
+		TranscriptPath: "",
+		ConversationID: "",
+		SessionID:      "",
+		CWD:            "",
+	}
+	_ = store.Save(context.Background(), key, "")
+	body, ok, err := store.Consume(context.Background(), key)
+	_ = body
+	_ = ok
+	_ = err
+=======
+	_ = shellCommand
+	_ = shellQuote
+	_ = isShellBareWord
+	_ = SupportedClients
+	_ = writeAdditionalContext
+	_ = writeCursorFollowup
+	_ = NewFileSnapshotStore
+	_ = FileSnapshotStore.Save
+	_ = FileSnapshotStore.Consume
+	_ = normalizeSnapshotKey
+	_ = marshalCodexHookInstalls
+	_ = removeCodexCommandHookGroups
+	_ = isCodexHookGroupHeader
+	_ = isNonHookTableHeader
+	_ = codexGroupContainsCommand
+	_ = parseTomlAssignment
+	_ = removeCodexManagedBlock
+	_ = ensureCodexHooksFeature
+	_ = isTomlTableHeader
+	_ = renderCodexManagedBlock
+	_ = codexCommandSignatures
+	_ = codexEventName
+	_ = marshalCodexTrustedHash
+	_ = unmarshalCursorHooksDocument
+	_ = (*cursorHooksDocument).MarshalJSON
+	_ = (*cursorHooksDocument).marshalCursorHookInstalls
+	_ = (*cursorHooksDocument).unmarshalCursorHooks
+	_ = (*cursorHooksDocument).setInt
+	_ = cursorCommandSignatures
+	_ = removeCursorHookHandlers
+	_ = newCursorCommandHookHandler
+	_ = (*rawCursorHookHandler).MarshalJSON
+	_ = (*rawCursorHookHandler).UnmarshalJSON
+	_ = (*rawCursorHookHandler).command
+	_ = (*rawCursorHookHandler).setString
+	_ = (*rawCursorHookHandler).setInt
+	_ = (*rawCursorHookHandler).setBool
+)
+
+func init() {
+	_ = shellCommand("", nil)
+	_ = SupportedClients()
+	_ = writeAdditionalContext(io.Discard, ClientClaudeCode, "", "")
+	_ = writeCursorFollowup(io.Discard, "")
+
+	store := NewFileSnapshotStore(filepath.Join(os.TempDir(), "clyde-hookspec-reachability"))
+	key := ReorientSnapshotKey{
+		TranscriptPath: "",
+		ConversationID: "",
+		SessionID:      "",
+		CWD:            "",
+	}
+	_ = store.Save(context.Background(), key, "")
+	body, ok, err := store.Consume(context.Background(), key)
+	_ = body
+	_ = ok
+	_ = err
+>>>>>>> 86f605bf (Fix hookspec sentinel pointer method references)
 	_, _ = marshalCodexHookInstalls(nil, nil, nil, "", "")
 	_    = stackRefsCursorDocument.marshalCursorHookInstalls
 	_    = stackRefsCursorDocument.MarshalJSON
