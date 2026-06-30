3452a5b24179abc6644e89c52f52d91cf3cfbb24
Alexander Goodkind <alex@goodkind.io>
2026-06-30 10:22:57 -0700

    Reference output and snapshot helper functions

--- internal/hookspec/stack_refs.go
+++ internal/hookspec/stack_refs.go
@@ -5,6 +5,12 @@ var (
 	_ = shellQuote
 	_ = isShellBareWord
 	_ = SupportedClients
+	_ = writeAdditionalContext
+	_ = writeCursorFollowup
+	_ = NewFileSnapshotStore
+	_ = FileSnapshotStore.Save
+	_ = FileSnapshotStore.Consume
+	_ = normalizeSnapshotKey
 )
 
 func init() {
